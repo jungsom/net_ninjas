2f1319156159aba10c0c7c5feb8056d6313a3d92
[AI10] 김나은 <naeunkim.business@gmail.com>
2024-06-06 20:24:47 +0900

    Add Header component to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Analysis from "./components/Analysis";
+
 function App() {
-  return <div className="App"></div>;
+
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          {/* <Route path="/total" element={<Total />} /> */}
+          <Route path="/analysis" element={<Analysis />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/Header.js
@@ -0,0 +1,39 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function CollapsibleMenu() {
+  return (
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">동네잇유</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Home</Nav.Link>
+            <NavDropdown title="통계" id="collapsible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">전체 통계</NavDropdown.Item>
+              <NavDropdown.Item href="analysis">
+                통계 분석
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#pricing">추천</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">로그인</Nav.Link>
+            {/* <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+            </Nav.Link> */}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default CollapsibleMenu;
