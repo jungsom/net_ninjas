50b06b44522fc8d6b25bc2076eda59968f6d1243
[AI10] 정유경 <jungyk0414@naver.com>
2024-06-07 21:44:15 +0900

    파일 위치 재수정

--- index.js
+++
@@ -1,9 +0,0 @@
-import app from "./app.js";
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => console.log(`서버가 ${port}번 포트에서 실행 중입니다.`));
-
-app.get("/", (req, res) => {
-    res.send("Home Page");
-  });
