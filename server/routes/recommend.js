04d47c1c6f70d641497edb720184b2c0a348f288
[AI10] 정유경 <jungyk0414@naver.com>
2024-06-08 02:46:31 +0900

    feat: 추천 api - 우선순위 받기

--- server/routes/recommend.js
+++ server/routes/recommend.js
@@ -5,7 +5,9 @@ import Housing from "../models/housing.js";
 import Population from "../models/population.js";
 import Region from "../models/safety.js";
 import Transportation from "../models/transportation.js"; 
-// import Welfare from "../models/welfare.js"
+import Welfare from "../models/welfare.js"
+import Environment from "../models/environment.js"
+import Safety from "../models/safety.js"
 
 const recommendRouter = Router();
 
@@ -13,28 +15,30 @@ recommendRouter.get('/', async (req, res, next) => {
     const { first, second, third, min_price, max_price } = req.query;
 
     const category = {
-        convenience,
-        education,
-        environment,
-        housing,
-        population,
-        region,
-        safety,
-        transportation,
-        welfare
+        convenience: Convenience,
+        education: Education,
+        environment: Environment,
+        housing: Housing,
+        population: Population,
+        region: Region,
+        safety: Safety,
+        transportation: Transportation,
+        welfare: Welfare
     };
 
-    if (category[first] & category[second] & category[third]) {
-        first = category[first];
-        second = category[second];
-        third = category[third];
-    }
+    const firstModel = category[first];
+    const secondModel = category[second];
+    const thirdModel = category[third];
 
-    const firstData = await first.find().lean();
-    const secondData = await second.find().lean();
-    const thirdData = await third.find().lean();
+    const firstData = await firstModel.find().lean();
+    const secondData = await secondModel.find().lean();
+    const thirdData = await thirdModel.find().lean();
 
-    res.send(firstData, secondData, thirdData)
+    res.send({
+        first: firstData,
+        second: secondData,
+        third: thirdData
+    });
 })
 
 export default recommendRouter;
