2a3971cf3aef4152744475cf47ebd2acb578f156
seona <osa746@gmail.com>
2024-06-09 23:21:58 +0900

    특정 동별 조회 기능 : allResearch/search로 변경

--- server/routes/allResearch.js
+++ server/routes/allResearch.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import { allDataPerPage } from '../controllers/allResearchController.js';
+import { searchData } from '../controllers/allResearchSearchController.js';
+
 const allResearchRoutes = express.Router();
 
 // 전체 데이터 페이지별 조회
 allResearchRoutes.get('/', allDataPerPage);
+allResearchRoutes.get('/search', searchData);
 
 export default allResearchRoutes;
--- server/routes/researchSearch.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { searchData } from '../controllers/researchSearchController.js';
-const router = express.Router();
-
-router.get('/', searchData);
-
-export default router;
