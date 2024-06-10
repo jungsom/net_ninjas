d6a40be06bdc2deb6737bd1c04abcccc5cf87810
civica23@gmail.com <civica23@gmail.com>
2024-06-10 17:04:24 +0900

    Feature: 임시 메뉴 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
 import Analysis from './components/Analysis';
 import Total from './components/Total';
 
 function App() {
-  return <Total />;
-  // return <Analysis />;
+  return (
+    <div className='App'>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/total' element={<Total />} />
+          <Route path='/analysis' element={<Analysis />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
