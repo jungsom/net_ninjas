6d2d3ffb9a4e43eec42332d24f581687943a6b58
seona <osa746@gmail.com>
2024-06-11 01:38:33 +0900

    카테고리별로 필드 데이터 리턴해주는 서비스 추가

---
+++ server/services/categorizationService.js
@@ -0,0 +1,69 @@
+import Region from '../models/region.js';
+
+export async function allConvenienceData() {
+ return await generateDataByFields(['supermarket']);
+}
+
+export async function allEducationData() {
+  return await generateDataByFields(['libraryCount', 'academyCount']);
+ }
+
+ export async function allEnvironmentData() {
+  return await generateDataByFields(['parkRate']);
+ }
+
+ export async function allHousingData() {
+  return await generateDataByFields(['jeonseDeposit', 'monthDeposit', 'monthRent']);
+ }
+
+ export async function allPopulationData() {
+  return await generateDataByFields(['youthRate']);
+ }
+
+ export async function allSafetyData() {
+  return await generateDataByFields(['crimeRate']);
+ }
+
+ export async function allTransportationData() {
+  return await generateDataByFields(['busStation']);
+ }
+
+ export async function allWelfareData() {
+  return await generateDataByFields(['cultureCount', 'medicalCount']);
+ }
+
+ export async function allData() {
+  const regions = await getRegions();
+  console.log(regions);
+  return regions;
+ }
+
+async function generateDataByFields(fields){
+  const regions = await getRegions();
+  const data = {};
+
+  regions.forEach((region) => {
+    const id = region._id;
+    data[id] = {
+      id: id,
+      gu: region.gu,
+      dong: region.dong,
+    };
+
+    fields.forEach((field) => {
+      data[id][field] = region[field];
+    });
+  });
+
+  return Object.values(data);
+}
+
+async function getRegions() {
+  try {
+    return await Region.find().lean();
+
+  } catch (error) {
+    const err = new Error('지역 데이터를 불러오는 중 오류가 발생했습니다.');
+    next(err);
+  }
+}
