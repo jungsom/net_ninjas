f3b35d0f641d87354e70a7be64db9ca2cf8592a8
seona <osa746@gmail.com>
2024-06-11 21:09:05 +0900

    유저스키마 추가, jwt토큰을 이용한 로그인을 하기 위한 디펜던시 추가

---
+++ server/models/user.js
@@ -0,0 +1,24 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  }
+},
+{
+  collection: 'users',
+  timestamps: true,
+});
+
+const User = model('User', userSchema);
+
+export default User;
