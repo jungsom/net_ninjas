0d54cbcdb7b7b7661db12d0131b2fbe9dedcacbd
[AI10] 김영선 <ysus05@naver.com>
2024-06-17 18:53:43 +0900

    Fix: 프론트 연결시 로그인 에러 해결

--- server/app.js
+++ server/app.js
@@ -21,7 +21,12 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    credentials: true
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
