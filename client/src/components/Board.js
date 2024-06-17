d979f31ae81736ee77a4e262656f9c6f587019b1
civica23@gmail.com <civica23@gmail.com>
2024-06-17 19:29:34 +0900

    Update: prettierrc 추가

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -1,13 +1,29 @@
 import BoardMainPage from './board/BoardMainPage';
 import BoardSearchBar from './board/BoardSearchBar';
+import styled from 'styled-components';
 
 function Board() {
   return (
-    <>
-      <BoardSearchBar />
-      <BoardMainPage />
-    </>
+    <StyledCenterLayout>
+      <StyledContent>
+        <BoardSearchBar />
+        <BoardMainPage />
+      </StyledContent>
+    </StyledCenterLayout>
   );
 }
+
+const StyledCenterLayout = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const StyledContent = styled.div`
+  width: 1600px;
+  h2 {
+    margin-top: 30px;
+    text-align: center;
+  }
+`;
 
 export default Board;
--- client/src/components/board/BoardSearchBar.js
+++ client/src/components/board/BoardSearchBar.js
@@ -1,3 +1,43 @@
-function BoardSearchBar() {}
+import InputBox from '../InputBox';
+import styled from 'styled-components';
+import { useState } from 'react';
+import { Search } from 'react-bootstrap-icons';
+
+function BoardSearchBar() {
+  const [hashtag, setHashtag] = useState('');
+  return (
+    <BoardSearchLayout>
+      <div>
+        <h4>
+          <strong>동잇스타그램</strong>
+        </h4>
+        <div>동네 사진을 올려주세요!</div>
+      </div>
+      <div></div>
+      <SearchInputBox>
+        <Search />
+        <InputBox
+          name='boardSearch'
+          value={hashtag}
+          placeholder='해시태그 검색'
+          onChange={(e) => setHashtag(e.target.value)}
+        />
+      </SearchInputBox>
+    </BoardSearchLayout>
+  );
+}
+
+const BoardSearchLayout = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  gap: 20px;
+  margin-top: 40px;
+`;
+
+const SearchInputBox = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
 
 export default BoardSearchBar;
