4d73181555a078a206f5da6e71a765eaa1b24e66
[AI10] 김영선 <ysus05@naver.com>
2024-06-19 19:01:49 +0900

    Fix: 코드 수정

--- server/controllers/allResearchSearchController.js
+++ server/controllers/allResearchSearchController.js
@@ -4,37 +4,13 @@ import {
   paginateData
 } from '../services/allResearchService.js';
 
-function isSameRegion(keywords, gu, dong) {
+function isGuDong(keywords, gu, dong) {
   return keywords.some((keyword) => {
     let str = keyword.split(' ');
     if (str[0] == gu && str[1] == dong) return true;
 
     return false;
   });
-}
-
-const { keyword, column, sorting } = req.query;
-const keywords = keyword.split(',').map((k) => k.trim());
-
-console.log('keywords : ', keywords);
-
-try {
-  let data = (await getAllData()).filter((t) => {
-    if (
-      keywords.some((k) => t.gu.includes(k)) ||
-      keywords.some((k) => t.dong.includes(k)) ||
-      isSameRegion(keywords, t.gu, t.dong)
-    )
-      return true;
-  });
-
-  data = sortData(data, column, sorting);
-
-  const paginatedData = paginateData(data, perPage, pageNo);
-
-  res.json(paginatedData);
-} catch (error) {
-  next(error);
 }
 
 export async function searchData(req, res, next) {
@@ -45,7 +21,12 @@ export async function searchData(req, res, next) {
 
   try {
     let data = (await getAllData()).filter((t) => {
-      if (includesSameRegion(keywords, `${t.gu} ${t.dong}`)) return true;
+      if (
+        keywords.some((k) => t.gu.includes(k)) ||
+        keywords.some((k) => t.dong.includes(k)) ||
+        isGuDong(keywords, t.gu, t.dong)
+      )
+        return true;
     });
 
     data = sortData(data, column, sorting);
